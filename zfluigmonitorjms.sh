ada563707c39c190a683e2152263d2ff056a7614
Rafael Benites Gil <rafaelbgil@users.noreply.github.com>
2016-03-02 07:43:11 -0300

    Update zfluigmonitorjms.sh

--- zfluigmonitorjms.sh
+++ zfluigmonitorjms.sh
@@ -2,10 +2,10 @@
 #Monitoramento Filas JMS
 #Autor: Rafael Benites Gil
 
-JBOSS_CLI="/app/fluig/jboss/bin/jboss-cli.sh"
-IP_JBOSS="172.16.102.224"
-PORTA_JBOSS="9999"
-JAVA_HOME="/app/fluig/jdk-64/"
+JBOSS_CLI="/fluig/jboss/bin/jboss-cli.sh" # Informar onde se encontra o executavel jboss-cli.sh .
+IP_JBOSS="127.0.0.1" # Informar o ip da interface de gerenciamento do jboss.
+PORTA_JBOSS="9999" # Informar porta de gerenciamento do jboss.
+JAVA_HOME="/fluig/jdk-64/" # Informar o diretorio da instalacao do java, recomendacao: informar o diretorio jdk que fica dentro do diretorio de instalacao do fluig, ex: /fluig/jdk-64
 PATH=$JAVA_HOME/bin:$PATH
 export PATH JAVA_HOME
 case $1 in
@@ -60,5 +60,4 @@ case $1 in
 esac
 			
 retorno=$(sh $JBOSS_CLI --connect --controller=$IP_JBOSS:$PORTA_JBOSS --commands="/subsystem=messaging/hornetq-server=default/jms-queue=$fila:read-resource(include-runtime=true)" | grep -i message-count | grep -o "[0-9]*")
-#echo "Fila: $fila , Mensagens: $retorno"
 echo $retorno
